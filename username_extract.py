dde8b8532eeab55010f8a19dfc15aaac1e9d8527
Harrelix <phunglegend.2004@gmail.com>
2023-05-24 12:45:31 +0700

    implement exponential backoff

--- username_extract.py
+++ username_extract.py
@@ -4,8 +4,13 @@ import os
 import sys
 import time
 
-STOPPING_POINT = 900
+STOPPING_POINT = 10000
 USERNAMES_FILE = "data/usernames.txt"
+
+# exponential backoff parameters
+INIT_WAIT_TIME = 1
+WAIT_TIME_MULTIPLIER = 2
+MAX_WAIT_TIME = 60
 
 
 def main():
@@ -17,10 +22,37 @@ def main():
     num_refresh = 0  # Counter for the number of times the page is refreshed
     start_time = time.time()  # Record the starting time of the process
 
+    wait_time = -1
+    total_wait_time = 0
+
     # Scraping loop: continue until enough usernames are collected
     while len(usernames) < STOPPING_POINT:
+        if wait_time > 0:
+            sys.stdout.write(f"Waiting {wait_time}s for next request\r")
+            sys.stdout.flush()
+            time.sleep(wait_time)
+            total_wait_time += wait_time
+
         # Send a GET request to the users URL
         response = requests.get(users_url)
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            # request failed, update wait timer
+            if wait_time < 0:
+                print(f"\nRequest failed, error code {response.status_code}")
+                wait_time = INIT_WAIT_TIME
+            else:
+                wait_time = min(wait_time * WAIT_TIME_MULTIPLIER, MAX_WAIT_TIME)
+            continue
+
+        # request succeeded, reset wait timer
+        if wait_time > 0:
+            wait_time = -1
+            print(f"Request succeeded after waiting {total_wait_time}s")
+            total_wait_time = 0
+
         html_content = response.text
 
         # Parse the HTML content using BeautifulSoup
@@ -64,9 +96,9 @@ def main():
     usernames -= existing_usernames
 
     # Append new usernames to the existing file
-    with open(USERNAMES_FILE, "a", encoding="utf8") as file:
-        file.write("\n".join(usernames))
-
+    with open(USERNAMES_FILE, "a", encoding="utf8") as f:
+        for username in usernames:
+            f.write(username + "\n")
 
 if __name__ == "__main__":
     main()
